34deffdaac6b81d8c0305c352908eb54f0582dc2
Radamir19 <radamir33@mail.ru>
2022-10-23 22:15:27 +0300

    self made

--- java_core1/Task_10.java
+++ java_core1/Task_10.java
@@ -1,45 +1,32 @@
 package java_core1;
 
+
 import java.util.*;
 
 public class Task_10 {
     public static void main(String[] args) {
-        Phonebook.secondMethod();
-        Work.firstMethod();
+        taskOne();
+        PhoneBook.taskTwo();
+    }
+
+    public static void taskOne() {
+        String[] strings = {"Hello", "Hola", "Hi", "Sasha", "Hi", "Alex", "Hola", "George", "Alex", "Night"};
+        Map<String, Integer> hm = new HashMap<>();
+        for (String string: strings) {
+            hm.put(string,hm.getOrDefault(string,0) + 1);
+        }
+        System.out.println(hm);
     }
 }
 
-class Work
-{
-    public static void firstMethod(){
-            Map<String, Integer> hm = new HashMap<>();
-            String[] words = {
-                    "Coffee", "Potato", "Alpha",
-                    "Cheese", "Beta", "Humster",
-                    "Dog", "Cat", "Java",
-                    "Yava", "Kent", "Coffee",
-                    "Dog", "Beta", "Humster",
-                    "Cat", "Java", "Yava", "Dog"
-            };
-
-            for (int i = 0; i < words.length; i++) {
-                if (hm.containsKey(words[i]))
-                    hm.put(words[i], hm.get(words[i]) + 1);
-                else
-                    hm.put(words[i], 1);
-            }
-            System.out.println(hm);
-    }
-}
-
-class Phonebook{
-    public static void secondMethod() {
-        Map<String, List<String>> name = new LinkedHashMap<>();
+class PhoneBook{
+    public static void taskTwo(){
+        Map<String,List<String>> name = new LinkedHashMap<>();
         List<String> phone = new ArrayList<>();
-        phone.add("8-977-132-21-22");
-        phone.add("8-977-132-21-20");
-        name.put("Petr",phone);
+        phone.add("8-999-888-77-66");
+        phone.add("8-999-777-66-55");
+        name.put("Alex",phone);
 
-        System.out.println(name.get("Petr"));
+        System.out.println(name.get("Alex"));
     }
 }
